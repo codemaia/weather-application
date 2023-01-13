e6dfb66560e7b959eeefd183d66141923383141b
Renan Maia <renanmaia.rl@gmail.com>
2023-01-12 22:12:56 -0300

    coding app.js. God is good!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const cityForm = document.querySelector('[data-js="change-location"]');
+const cityName = document.querySelector('[data-js="city-name"]');
+const cityWeather = document.querySelector('[data-js="city-weather"]');
+const cityTemperature = document.querySelector('[data-js="city-temperature"]');
 
 cityForm.addEventListener('submit', async event => {
 
@@ -9,9 +12,11 @@ cityForm.addEventListener('submit', async event => {
     const { Key, LocalizedName } = await getCityData(inputValueCity);
     const { WeatherText, Temperature } = await getCityWeather(LocalizedName);
 
-    
+    cityName.innerHTML = LocalizedName;
+    cityWeather.innerHTML = WeatherText;
+    cityTemperature.innerHTML = Temperature.Metric.Value;
 
-    console.log(WeatherText, Temperature.Metric.Value);
+    console.log();
     cityForm.reset();
 
 });
